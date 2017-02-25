5588f687ffbed6b409c1931fffa8ec76050ae02f
Eduardo Hitek <eduardohitek@gmail.com>
2017-02-25 12:49:44 -0300

    First Custom Module

--- 2_example/app.js
+++ 2_example/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var msg = require('./mod_teste')();
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -17,5 +18,5 @@ app.get('/noticias', function(req, res) {
 
 
 app.listen(3000, function(){
-  console.log("Servidor rodando com EJS!");
+  console.log(msg);
 });
---
+++ 2_example/mod_teste.js
@@ -0,0 +1,4 @@
+module.exports = function(){
+  var msg = "Esse modulo contém apenas uma String";
+  return msg;
+};
