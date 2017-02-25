3a4044cf1bd127fece0a62d0a47142f618988788
Eduardo Hitek <eduardohitek@gmail.com>
2017-02-25 08:28:24 -0300

    small changes

--- 2_example/noticias.js
+++ 2_example/noticias.js
@@ -1,18 +1,20 @@
 var http = require('http');
 
-var server = http.createServer( function(req, res){
 
-  var categoria = req.url;
 
-  if(categoria == '/tecnologia'){
-    res.end("<html><body>Notícias de Tecnologia</body></html>");
-  } else if (categoria == '/moda'){
-    res.end("<html><body>Notícias de Moda</body></html>");
-  } else if (categoria == '/beleza'){
-    res.end("<html><body>Notícias de Beleza</body></html>");
-  } else {
-    res.end("<html><body>Portal de Notícias</body></html>");
-  }
+var server = http.createServer(function(req, res) {
+
+    var categoria = req.url;
+
+    if (categoria == '/tecnologia') {
+        res.end("<html><body>Notícias de Tecnologia</body></html>");
+    } else if (categoria == '/moda') {
+        res.end("<html><body>Notícias de Moda</body></html>");
+    } else if (categoria == '/beleza') {
+        res.end("<html><body>Notícias de Beleza</body></html>");
+    } else {
+        res.end("<html><body>Portal de Notícias</body></html>");
+    }
 
 });
 
