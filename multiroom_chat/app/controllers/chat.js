7ca20fa27f1d4f9332becf463b3f2f18b8b06480
Eduardo Hitek <eduardohitek@gmail.com>
2017-03-10 19:35:48 -0300

    Form validation

--- multiroom_chat/app/controllers/chat.js
+++ multiroom_chat/app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.iniciaChat = function(application, req, res){
+  let dadosForm = req.body;
+
+  req.assert('apelido', 'Nome ou Apelido é Obrigatório').notEmpty();
+  req.assert('apelido', 'Nome ou Apelido deve ter entre 3 e 15 caracteres').len(3, 15);
+
+  let erros = req.validationErrors();
+
+  if(erros){
+    res.render("index", {validacao : erros})
+    return;
+  }
+
   res.render("chat");
 }
