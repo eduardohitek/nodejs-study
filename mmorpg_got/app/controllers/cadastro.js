d7448ef162b6caba59f96187cb8fd0a499186ff5
Eduardo Hitek <eduardohitek@gmail.com>
2017-03-12 22:16:08 -0300

    Form validation

--- mmorpg_got/app/controllers/cadastro.js
+++ mmorpg_got/app/controllers/cadastro.js
@@ -1,3 +1,22 @@
 module.exports.cadastro = function(application, req, res){
-  res.render('cadastro');
+  res.render('cadastro', {validacao: {}, dadosForm: {}});
+}
+
+
+module.exports.cadastrar = function(application, req, res){
+  let dadosForm = req.body;
+
+  req.assert('nome', 'Nome não pode ser Vazio').notEmpty();
+  req.assert('usuario', 'Usuário não pode ser Vazio').notEmpty();
+  req.assert('senha', 'Senha não pode ser Vazio').notEmpty();
+  req.assert('casa', 'Casa não pode ser Vazio').notEmpty();
+
+  let erros = req.validationErrors();
+
+  if(erros){
+    res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+    return
+  }
+
+  res.send("Teste Cadastro!");
 }
