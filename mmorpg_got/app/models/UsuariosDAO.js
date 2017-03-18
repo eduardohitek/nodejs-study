e212548c9e9b4e31e51ff211034ad42037818b68
Eduardo Hitek <eduardohitek@gmail.com>
2017-03-18 18:39:50 -0300

    Using Crypto for Users Password

--- mmorpg_got/app/models/UsuariosDAO.js
+++ mmorpg_got/app/models/UsuariosDAO.js
@@ -1,39 +1,50 @@
-function UsuariosDAO(connection){
-  this._connection = connection();
+let crypto = require('crypto')
+
+function UsuariosDAO(connection) {
+    this._connection = connection();
 }
 
-UsuariosDAO.prototype.inserirUsuario = function(usuario){
-  this._connection.open(function(err, mongoclient){
-    mongoclient.collection("usuarios", function(err, collection){
-      collection.insert(usuario);
+UsuariosDAO.prototype.inserirUsuario = function(usuario) {
+    this._connection.open(function(err, mongoclient) {
+        mongoclient.collection("usuarios", function(err, collection) {
+            let senha_crypto = crypto.createHash("md5")
+                .update(usuario.senha).digest("hex")
+            usuario.senha = senha_crypto
+            collection.insert(usuario);
 
-      mongoclient.close();
+            mongoclient.close();
+        });
     });
-  });
 }
 
-UsuariosDAO.prototype.autenticar = function(usuario, req, res){
-  this._connection.open(function(err, mongoclient){
-    mongoclient.collection("usuarios", function(err, collection){
-      collection.find(usuario).toArray(function(err, result){
-        if(result[0] != undefined){
-          req.session.autorizado = true;
-          req.session.usuario = result[0].usuario;
-          req.session.casa = result[0].casa;
-        }
+UsuariosDAO.prototype.autenticar = function(usuario, req, res) {
+    this._connection.open(function(err, mongoclient) {
+        mongoclient.collection("usuarios", function(err, collection) {
+            let senha_crypto = crypto.createHash("md5")
+                .update(usuario.senha).digest("hex")
+            usuario.senha = senha_crypto
+            collection.find(usuario).toArray(function(err, result) {
+                if (result[0] != undefined) {
+                    req.session.autorizado = true;
+                    req.session.usuario = result[0].usuario;
+                    req.session.casa = result[0].casa;
+                }
 
-        if(req.session.autorizado){
-          res.redirect('jogo');
-        } else {
-          res.render('index', {validacao:{}, dadosForm:{}});
-        }
-      });
+                if (req.session.autorizado) {
+                    res.redirect('jogo');
+                } else {
+                    res.render('index', {
+                        validacao: {},
+                        dadosForm: {}
+                    });
+                }
+            });
 
-      mongoclient.close();
+            mongoclient.close();
+        });
     });
-  });
 }
 
-module.exports = function(){
-  return UsuariosDAO;
+module.exports = function() {
+    return UsuariosDAO;
 }
